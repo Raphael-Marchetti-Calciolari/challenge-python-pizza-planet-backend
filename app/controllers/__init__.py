0587f05f6306ae9203e08375b5f251ee85954ab6
@Raphael-Marchetti-Calciolari <rapha.marchetti@gmail.com>
2024-08-15 16:36:21 -0300

    Created new model for beverages and integrated on the system

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,5 +1,6 @@
 from .index import IndexController
 from .ingredient import IngredientController
+from .beverage import BeverageController
 from .order import OrderController
 from .size import SizeController
 from .mock import MockController
